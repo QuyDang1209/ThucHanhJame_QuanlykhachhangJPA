04e473bd430dccee752bd2e7a5ba7575acbaaeea
QuyDang1209 <dangphuocquy1996@gmail.com>
2024-05-20 11:23:50 +0700

    thuc hanhh quan ly khach hang JPA

---
+++ src/main/java/com/cg/thuchanhquanlykhachhangjpa/repository/ICustomerRepository.java
@@ -0,0 +1,6 @@
+package com.cg.thuchanhquanlykhachhangjpa.repository;
+
+import com.cg.thuchanhquanlykhachhangjpa.model.Customer;
+
+public interface ICustomerRepository extends IGenerateRepository<Customer> {
+}
---
+++ src/main/java/com/cg/thuchanhquanlykhachhangjpa/service/ICustomerService.java
@@ -0,0 +1,6 @@
+package com.cg.thuchanhquanlykhachhangjpa.service;
+
+import com.cg.thuchanhquanlykhachhangjpa.model.Customer;
+
+public interface ICustomerService extends IGenerateService<Customer>{
+}
